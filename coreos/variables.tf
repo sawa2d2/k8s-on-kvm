da767706f26a703123e2ea9063df760b3523b3b4
sawa2d2 <sawa.7c9@hotmail.com>
2023-03-07 07:44:59 +0900

    [remove]  remove variables.tfvars

--- coreos/variables.tf
+++ coreos/variables.tf
@@ -1,1 +1,3 @@
-variable coreos_image_uri {}
+variable coreos_image_uri {
+    default = "/var/lib/libvirt/images/fedora-coreos-37.20230205.3.0-qemu.x86_64.qcow2"
+}
--- coreos/variables.tfvars
+++
@@ -1,1 +0,0 @@
-coreos_image_uri = "/var/lib/libvirt/images/fedora-coreos-37.20230205.3.0-qemu.x86_64.qcow2"
