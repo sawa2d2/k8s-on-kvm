86a9dd4b3adc7daec96ff86a928b96af55b9cbc0
sawa2d2 <sawa.7c9@hotmail.com>
2023-10-23 20:00:08 +0900

    [update]  enable to get IPs dynamically

--- kubernetes/test.sh
+++ kubernetes/test.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
 # Check if enable to login via ssh
-ip_list=( \
-    "192.168.8.201" \
-    "192.168.8.202" \
-    "192.168.8.203" \
-    "192.168.8.204" \
-)
+ip_list=($(./generate_inventory.py | jq --raw-output '._meta.hostvars[].ip' | xargs))
 for ip in "${ip_list[@]}"; do
     echo "Connecting $ip ..."
     ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$ip pwd
