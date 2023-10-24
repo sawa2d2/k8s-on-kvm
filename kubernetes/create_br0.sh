d1a4d736cd484100bed84cdf679228ece506e220
sawa2d2 <sawa.7c9@hotmail.com>
2023-10-24 17:06:52 +0900

    [update]  create_br0.sh

--- kubernetes/create_br0.sh
+++ kubernetes/create_br0.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
 # Check the network interface of the host
-HOST_IP=192.168.8.12
-NWIF=$(ip a | grep $HOST_IP | awk '{ print $8}')
-echo "The network interface of the host is $NWIF."
+HOST_IP=192.168.8.10
+GATEWAY=192.168.8.1
+DNS=192.168.8.1
+
+nwif=$(ip a | grep $HOST_IP | awk '{ print $8}')
+echo "The network interface of the host is $nwif."
 
 # Create a temp bridge br0 if not exists
 
-if [[ $(brctl show | grep -E '^br0\s+' > /dev/null 2>&1; echo $?) ]]; then
+if [[ $(nmcli | grep -E '^br0\s+' > /dev/null 2>&1; echo $?) ]]; then
   echo "br0 is already exists."
 else
   echo "Creating br0..."
-  sudo brctl addbr br0
+  nmcli con add type bridge ifname br0
 fi
-sudo ip link set up dev br0
-sudo ip addr add dev br0 192.168.8.8/24
-echo "Adding $NWIF to br0..."
-sudo brctl addif br0 $NWIF
-brctl show
+
+nmcli connection modify bridge-br0 \
+ipv4.method manual \
+ipv4.addresses "$HOST_IP/24" \
+ipv4.gateway "$GATEWAY" \
+ipv4.dns $DNS
+
+nmcli connection add type bridge-slave ifname $nwif master bridge-br0
+
+nmcli connection delete $nwif
