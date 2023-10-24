de4d6dafa0738edad00be4fd7985a3ef41fd2beb
sawa2d2 <sawa.7c9@hotmail.com>
2023-10-24 18:27:43 +0900

    [update]  add some error handling

--- kubernetes/create_br0.sh
+++ kubernetes/create_br0.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
-# Check the network interface of the host
 HOST_IP=192.168.8.10
+CIDR=24
 GATEWAY=192.168.8.1
 DNS=192.168.8.1
 
 nwif=$(ip a | grep $HOST_IP | awk '{ print $8}')
-echo "The network interface of the host is $nwif."
 
-# Create a temp bridge br0 if not exists
-
-if [[ $(nmcli | grep -E '^br0\s+' > /dev/null 2>&1; echo $?) ]]; then
-  echo "br0 is already exists."
-else
-  echo "Creating br0..."
-  nmcli con add type bridge ifname br0
+if [[ $nwif == 'br0' ]]; then
+  echo "Nothing to do: bridge \"$nwif\" is already exists."
+  echo "--------"
+  nmcli connection show
+  exit 0
+elif [[ $nwif == '' ]]; then
+  echo "Error: Exsisting network interface not found which has the address $HOST_IP/$CIDR."
+  exit 1
 fi
+
+echo "Creating br0..."
+nmcli con add type bridge ifname br0
+nmcli connection show
 
 nmcli connection modify bridge-br0 \
 ipv4.method manual \
-ipv4.addresses "$HOST_IP/24" \
+ipv4.addresses "$HOST_IP/$CIDR" \
 ipv4.gateway "$GATEWAY" \
 ipv4.dns $DNS
 
