df8ccdfd4176df8aa53df1cbc5111b016b41c673
sawa2d2 <sawa.7c9@hotmail.com>
2023-11-14 00:59:16 +0900

    [update]  libvirt module

--- rook-ceph/main.tf
+++ rook-ceph/main.tf
@@ -1,20 +1,21 @@
-module "kvm_cloudinit" {
-  source                  = "github.com/sawa2d2/terraform-modules//kvm-cloudinit/"
-  libvirt_uri             = "qemu:///system"
-  vm_base_image_uri       = "/var/lib/libvirt/images/Rocky-9-GenericCloud.latest.x86_64.qcow2"
-  virtual_bridge          = "br0"
-  gateway                 = "192.168.8.1"
-  nameservers             = "[\"192.168.8.1\"]"
-  cloud_init_cfg_path     = "${path.module}/cloud_init.cfg"
+module "ceph_rook" {
+  source = "github.com/sawa2d2/terraform-modules//libvirt-cloudinit-bridge/"
+
+  libvirt_uri       = "qemu:///system"
+  vm_base_image_uri = "/var/lib/libvirt/images/Rocky-9-GenericCloud.latest.x86_64.qcow2"
+  bridge            = "br0"
+  gateway           = "192.168.8.1"
+  nameservers       = ["192.168.8.1"]
   vms = [
     {
-      name        = "storage-1"
-      vcpu        = 4
-      memory      = 16000                    # in MiB
-      disk        = 100 * 1024 * 1024 * 1024 # 100 GB
-      ip          = "192.168.8.201/24"
-      mac         = "52:54:00:00:00:01"
-      description = <<-EOT
+      name           = "storage-1"
+      vcpu           = 4
+      memory         = 16000                    # in MiB
+      disk           = 100 * 1024 * 1024 * 1024 # 100 GB
+      ip             = "192.168.8.201/24"
+      mac            = "52:54:00:00:00:01"
+      cloudinit_file = "cloud_init.cfg"
+      description    = <<-EOT
       {
         "kubernetes": {
           "kube_control_plane": true,
@@ -26,22 +27,23 @@ module "kvm_cloudinit" {
       volumes = [
         {
           name = "vdb"
-          disk = 1024 * 1024 * 1024 * 1024 # 1 TB 
+          disk = 512 * 1024 * 1024 * 1024 # 500 GB
         },
         {
           name = "vdc"
-          disk = 1024 * 1024 * 1024 * 1024 # 1 TB 
+          disk = 512 * 1024 * 1024 * 1024 # 500 GB
         },
       ]
     },
     {
-      name        = "storage-2"
-      vcpu        = 4
-      memory      = 16000                    # in MiB
-      disk        = 100 * 1024 * 1024 * 1024 # 100 GB
-      ip          = "192.168.8.202/24"
-      mac         = "52:54:00:00:00:02"
-      description = <<-EOT
+      name           = "storage-2"
+      vcpu           = 4
+      memory         = 16000                    # in MiB
+      disk           = 100 * 1024 * 1024 * 1024 # 100 GB
+      ip             = "192.168.8.202/24"
+      mac            = "52:54:00:00:00:02"
+      cloudinit_file = "cloud_init.cfg"
+      description    = <<-EOT
       {
         "kubernetes": {
           "kube_control_plane": true,
@@ -58,13 +60,14 @@ module "kvm_cloudinit" {
       ]
     },
     {
-      name        = "storage-3"
-      vcpu        = 4
-      memory      = 16000                    # in MiB
-      disk        = 100 * 1024 * 1024 * 1024 # 100 GB
-      ip          = "192.168.8.203/24"
-      mac         = "52:54:00:00:00:03"
-      description = <<-EOT
+      name           = "storage-3"
+      vcpu           = 4
+      memory         = 16000                    # in MiB
+      disk           = 100 * 1024 * 1024 * 1024 # 100 GB
+      ip             = "192.168.8.203/24"
+      mac            = "52:54:00:00:00:03"
+      cloudinit_file = "cloud_init.cfg"
+      description    = <<-EOT
       {
         "kubernetes": {
           "kube_control_plane": false,
@@ -81,13 +84,14 @@ module "kvm_cloudinit" {
       ]
     },
     {
-      name        = "app"
-      vcpu        = 4
-      memory      = 16000                    # in MiB
-      disk        = 100 * 1024 * 1024 * 1024 # 100 GB
-      ip          = "192.168.8.204/24"
-      mac         = "52:54:00:00:00:04"
-      description = <<-EOT
+      name           = "app"
+      vcpu           = 4
+      memory         = 16000                    # in MiB
+      disk           = 100 * 1024 * 1024 * 1024 # 100 GB
+      ip             = "192.168.8.204/24"
+      mac            = "52:54:00:00:00:04"
+      cloudinit_file = "cloud_init.cfg"
+      description    = <<-EOT
       {
         "kubernetes": {
           "kube_control_plane": false,
@@ -96,7 +100,7 @@ module "kvm_cloudinit" {
         }
       }
       EOT
-      volumes     = []
+      volumes        = []
     },
   ]
 }
