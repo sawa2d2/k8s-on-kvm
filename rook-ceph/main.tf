cd2214958a8de7a79f144aa08421efdedf57a208
sawa2d2 <sawa.7c9@hotmail.com>
2023-11-14 23:29:23 +0900

    [update]  update module

--- rook-ceph/main.tf
+++ rook-ceph/main.tf
@@ -1,26 +1,30 @@
-module "ceph_rook" {
-  source = "github.com/sawa2d2/terraform-modules//libvirt-cloudinit-bridge/"
+module "kubernetes" {
+  source = "github.com/sawa2d2/k8s-on-kvm//kubernetes/"
 
-  libvirt_uri       = "qemu:///system"
+  # Localhost: "qemu:///system"
+  # Remote   : "qemu+ssh://<user>@<host>/system"
+  libvirt_uri = "qemu:///system"
+
+  # Download the image by:
+  #   sudo curl -L -o /var/lib/libvirt/images/Rocky-9-GenericCloud.latest.x86_64.qcow2 https://download.rockylinux.org/pub/rocky/9.2/images/x86_64/Rocky-9-GenericCloud.latest.x86_64.qcow2
   vm_base_image_uri = "/var/lib/libvirt/images/Rocky-9-GenericCloud.latest.x86_64.qcow2"
-  bridge            = "br0"
-  gateway           = "192.168.8.1"
-  nameservers       = ["192.168.8.1"]
-  vms = [
+  pool              = "default"
+
+  bridge      = "br0"
+  cidr        = "192.168.8.0/24"
+  gateway     = "192.168.8.1"
+  nameservers = ["192.168.8.1"]
+
+  nodes = [
     {
-      name           = "storage-1"
+      name           = "storage1"
       vcpu           = 4
       memory         = 16000                    # in MiB
       disk           = 100 * 1024 * 1024 * 1024 # 100 GB
-      ip             = "192.168.8.201/24"
-      mac            = "52:54:00:00:00:01"
+      ip             = "192.168.8.201"
+      mac            = "52:54:00:00:02:01"
       cloudinit_file = "cloud_init.cfg"
-      description    = <<-EOT
-        "kubernetes":
-          "kube_control_plane": true
-          "kube_node": true
-          "etcd": true
-      EOT
+      description    = ""
       volumes = [
         {
           name = "vdb"
@@ -31,64 +35,69 @@ module "ceph_rook" {
           disk = 512 * 1024 * 1024 * 1024 # 500 GB
         },
       ]
+
+      kube_control_plane = true
+      kube_node          = true
+      etcd               = true
     },
     {
-      name        = "storage-2"
-      vcpu        = 4
-      memory      = 16000                    # in MiB
-      disk        = 100 * 1024 * 1024 * 1024 # 100 GB
-      ip          = "192.168.8.202/24"
-      mac         = "52:54:00:00:00:02"
+      name           = "storage2"
+      vcpu           = 4
+      memory         = 16000                    # in MiB
+      disk           = 100 * 1024 * 1024 * 1024 # 100 GB
+      ip             = "192.168.8.202"
+      mac            = "52:54:00:00:02:02"
       cloudinit_file = "cloud_init.cfg"
-      description = <<-EOT
-        "kubernetes":
-          "kube_control_plane": true
-          "kube_node": true
-          "etcd": true
-      EOT
+      description    = ""
       volumes = [
         {
           name = "vdb"
           disk = 1024 * 1024 * 1024 * 1024 # 1 TB 
         },
       ]
+
+      kube_control_plane = false
+      kube_node          = true
+      etcd               = true
     },
     {
-      name        = "storage-3"
-      vcpu        = 4
-      memory      = 16000                    # in MiB
-      disk        = 100 * 1024 * 1024 * 1024 # 100 GB
-      ip          = "192.168.8.203/24"
-      mac         = "52:54:00:00:00:03"
+      name           = "storage3"
+      vcpu           = 2
+      memory         = 8000                     # in MiB
+      disk           = 100 * 1024 * 1024 * 1024 # 100 GB
+      ip             = "192.168.8.203"
+      mac            = "52:54:00:00:02:03"
       cloudinit_file = "cloud_init.cfg"
-      description = <<-EOT
-        "kubernetes":
-          "kube_control_plane": false
-          "kube_node": true
-          "etcd": true
-      EOT
+      description    = ""
       volumes = [
         {
           name = "vdb"
           disk = 1024 * 1024 * 1024 * 1024 # 1 TB 
         },
       ]
+
+      kube_control_plane = false
+      kube_node          = true
+      etcd               = false
     },
     {
-      name        = "app"
-      vcpu        = 4
-      memory      = 16000                    # in MiB
-      disk        = 100 * 1024 * 1024 * 1024 # 100 GB
-      ip          = "192.168.8.204/24"
-      mac         = "52:54:00:00:00:04"
+      name           = "app"
+      vcpu           = 2
+      memory         = 8000                     # in MiB
+      disk           = 100 * 1024 * 1024 * 1024 # 100 GB
+      ip             = "192.168.8.204"
+      mac            = "52:54:00:00:02:04"
       cloudinit_file = "cloud_init.cfg"
-      description = <<-EOT
-        "kubernetes":
-          "kube_control_plane": false
-          "kube_node": true
-          "etcd": false
-      EOT
-      volumes     = []
+      description    = ""
+      volumes        = []
+
+      kube_control_plane = true
+      kube_node          = true
+      etcd               = true
     },
   ]
+}
+
+output "kubespray_hosts" {
+  value = module.kubernetes.kubespray_hosts
 }
