a1f9e9c18ae0173d2d1c45ac0317aaebde62b0d9
sawa2d2 <sawa.7c9@hotmail.com>
2023-11-19 02:27:56 +0900

    [remove]  test.sh

--- kubernetes/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Check if enable to login via ssh
-ip_list=($(./generate_inventory.py | jq --raw-output '._meta.hostvars[].ip' | xargs))
-for ip in "${ip_list[@]}"; do
-    echo "Connecting $ip ..."
-    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$ip pwd
-done
