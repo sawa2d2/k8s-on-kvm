1def8fd6086cf280b3ee02257a4b70c31098b3a4
sawa2d2 <sawa.7c9@hotmail.com>
2025-01-21 22:23:27 +0900

    Fix main.tf

--- sno/main.tf
+++ sno/main.tf
@@ -9,14 +9,14 @@ terraform {
 
 provider "libvirt" {
   # Localhost:
-  #   libvirt_uri = "qemu:///system"
+  #   uri = "qemu:///system"
   # Remote:
-  #   libvirt_uri = "qemu+ssh://<user>@<remote-host>/system?keyfile=${pathexpand("~")}/.ssh/id_rsa&known_hosts_verify=ignore"
+  #   uri = "qemu+ssh://<user>@<remote-host>/system?keyfile=${pathexpand("~")}/.ssh/id_rsa&known_hosts_verify=ignore"
   # Remote via bastion:
   #   Forward port in advance:
   #     $ ssh -C -N -f -L 50000:<remote-user>@<remote-host>:22 <bastion-host> -p <bastion-port>
-  #   libvirt_uri = "qemu+ssh://<remote-user>@localhost:50000/system?keyfile=${pathexpand("~")}/.ssh/id_rsa&known_hosts_verify=ignore"
-  libvirt_uri = "qemu:///system"
+  #   uri = "qemu+ssh://<remote-user>@localhost:50000/system?keyfile=${pathexpand("~")}/.ssh/id_rsa&known_hosts_verify=ignore"
+  uri = "qemu:///system"
 }
 
 resource "libvirt_domain" "vm" {
